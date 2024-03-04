ff13e8195868d7efec09984ffd70c0ab336390ec
Arrunn Chhuoy <achhuoy@gmail.com>
2024-03-04 14:04:20 -0800

    init commit

---
+++ public/js/chat.js
@@ -0,0 +1,137 @@
+// COUNT EXAMPLE
+// const socket = io();
+
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('click');
+//     socket.emit('increment');
+// });
+
+const socket = io();
+
+// Message Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
+// Location elements
+const $locationButton = document.querySelector('#send-location');
+
+const $messages = document.querySelector('#messages');
+
+// Template
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector(
+    '#location-message-template'
+).innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild;
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight;
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+};
+
+socket.on('message', (message) => {
+    console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('MMMM DD YYYY h:mm A'),
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
+});
+
+socket.on('locationMessage', (location) => {
+    const html = Mustache.render(locationTemplate, {
+        username: location.username,
+        url: location.url,
+        createdAt: moment(location.createdAt).format('MMMM DD YYYY h:mm A'),
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users,
+    });
+
+    document.querySelector('#sidebar').innerHTML = html;
+});
+
+$messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled');
+    //disable
+
+    const message = event.target.elements.message.value;
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+
+        // enable
+
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message delivered successfully');
+    });
+});
+
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    $locationButton.setAttribute('disabled', 'disabled');
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit(
+            'sendLocation',
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            },
+            () => {
+                $locationButton.removeAttribute('disabled');
+                console.log('Location shared');
+            }
+        );
+    });
+});
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error);
+        location.href = '/';
+    }
+});
